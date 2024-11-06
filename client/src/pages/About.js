080be18579e47d1f37c3604d76ef2f559222ef2d
karthick palanisamy <karthikmass404@gmail.com>
2024-11-06 18:12:37 +0530

    first

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -3,7 +3,7 @@ import Layout from "./../components/Layout/Layout";
 
 const About = () => {
   return (
-    <Layout>
+    <Layout title={"About us - Ecommer app"}>
       <div className="row contactus ">
         <div className="col-md-6 ">
           <img
@@ -14,12 +14,7 @@ const About = () => {
         </div>
         <div className="col-md-4">
           <p className="text-justify mt-2">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus
-            officiis obcaecati esse tempore unde ratione, eveniet mollitia,
-            perferendis eius temporibus dicta blanditiis doloremque explicabo
-            quasi sunt vero optio cum aperiam vel consectetur! Laborum enim
-            accusantium atque, excepturi sapiente amet! Tenetur ducimus aut
-            commodi illum quidem neque tempora nam.
+          K.N. Garments is your one-stop destination for premium quality clothing and accessories. From trendy tops and stylish bottoms to cozy outerwear and chic accessories, we offer a wide range of fashion essentials to elevate your wardrobe. With a commitment to quality craftsmanship and the latest fashion trends, we strive to provide our customers with an unparalleled shopping experience. Whether you're looking for casual everyday wear or something for a special occasion, K.N. Garments has got you covered. Shop with us today and discover your perfect style statement!
           </p>
         </div>
       </div>
